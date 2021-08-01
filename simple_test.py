8691f4f8bb85794a35e7e60a9e17ad57e97497e5
lorryrui1981 <lrui@logitech.com>
2021-07-31 21:39:45 -0700

    combine sounddevice
    combine sounddevice

--- simple_test.py
+++ simple_test.py
@@ -3,7 +3,7 @@ filename='Device_Mic_THD_R_3.wav'
 trigeFrequncy=400  ## this need sweep from high(above 400) to low sweep tone
 stopananlysis=100   ## stop analyze_channels
 channaelselect=1 ### if recording is dual channel ,leftchannel=1, rightchannel=2, otherwise no need to define  
-freq,thdh,thd_N,power,Freq_THD,thd_data,Freq_Power,PowerS,RubBuzz_data=AUT.analyze_channels(filename, trigeFrequncy,stopananlysis,channaelselect) 
+freq,thdh,thd_N,power,Freq_THD,thd_data,Freq_Power,PowerS,RubBuzz_data=AUT.analyze_sweep(filename, trigeFrequncy,stopananlysis,channaelselect) 
 print('FFT Frequency:   %.1f Hz' % freq)
 print("Sweep Max THD:   %.4f%% " %thdh)
 print("Sweep Max THD+N: %.4f%%      Note, this is single tone use only " %thd_N)
